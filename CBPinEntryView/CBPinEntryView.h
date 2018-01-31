ce0839188a2e49fd292548459a395855da261182
Chris Ingram <chris@chrisingram.me>
2018-01-31 11:43:12 +0000

    created xcodeproj file and shared scheme

---
+++ CBPinEntryView/CBPinEntryView.h
@@ -0,0 +1,19 @@
+//
+//  CBPinEntryView.h
+//  CBPinEntryView
+//
+//  Created by Chris Ingram on 31/01/2018.
+//  Copyright © 2018 Chris Ingram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CBPinEntryView.
+FOUNDATION_EXPORT double CBPinEntryViewVersionNumber;
+
+//! Project version string for CBPinEntryView.
+FOUNDATION_EXPORT const unsigned char CBPinEntryViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CBPinEntryView/PublicHeader.h>
+
+
